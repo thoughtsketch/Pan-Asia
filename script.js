2242c2ad7ce212d0e4f06c252cd72426385fd9d5
Prathamesh Patil <prathameshp@thoughtsketch.com>
2025-05-02 12:54:06 +0530

    added_popup_form

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementById("contactModal").style.display = "flex";
+  };
+
+  function closeModal() {
+    document.getElementById("contactModal").style.display = "none";
+  }
